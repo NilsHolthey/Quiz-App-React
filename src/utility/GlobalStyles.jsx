f206eda4eefcd2e220c4619b1ae4e733eeef5c80
NilsHolthey <nils.holthey@gmx.de>
2022-04-15 12:53:10 +0200

    Add GlobalStyles

---
+++ src/utility/GlobalStyles.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background-color: #f8f8f8f8;
+  color: #969696;
+  font-family: "Montserrat", sans-serif;
+}
+`;
